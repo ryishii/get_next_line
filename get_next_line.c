42e52ccc6cebc18dbee512a471799619331458da
石井亮徳 <ishiiryoutoku@ishiiryoutokunoMacBook-Pro.local>
2021-03-24 03:31:10 +0900

    final '

--- get_next_line.c
+++ get_next_line.c
@@ -3,70 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryishii <ryishii@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: ishiiryoutoku <ishiiryoutoku@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 15:59:29 by ryishii           #+#    #+#             */
-/*   Updated: 2021/02/06 02:36:16 by ryishii          ###   ########.fr       */
+/*   Updated: 2021/03/24 03:26:06 by ishiiryouto      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int     safe_free(char **p, int res)
+int		safe_free(char **p, int res)
 {
-    if (*p != NULL)
-    {
-        free(*p);
-        *p = NULL;
-    }
-    return (res);
+	if (*p != NULL)
+	{
+		free(*p);
+		*p = NULL;
+	}
+	return (res);
 }
 
-int     put_line(char **line, char **save, char *buf)
+int		put_line(char **line, char **save, char *buf)
 {
-    char    *tmp;
-    size_t  len;
+	char    *tmp;
+	size_t  len;
 
-    len = ft_strchr(buf, '\n');
-    tmp = *line;
-    if (!(*line = ft_strjoin(*line, buf, len)))
-        return (safe_free(&tmp, -1));
-    safe_free(&tmp, 1);
-    if (buf[len] == '\n')
-    {
-        tmp = *save;
-        if (!(*save = ft_strdup(&buf[len + 1])))
-            return (safe_free(&tmp, -1));
-        return (safe_free(&tmp, 1));
-    }
-    return (safe_free(save, 0));
+	len = ft_strchr(buf, '\n');
+	tmp = *line;
+	if (!(*line = ft_strjoin(*line, buf, len)))
+		return (safe_free(&tmp, -1));
+	safe_free(&tmp, 1);
+	if (buf[len] == '\n')
+	{
+		tmp = *save;
+		if (!(*save = ft_strdup(&buf[len + 1])))
+			return (safe_free(&tmp, -1));
+		return (safe_free(&tmp, 1));
+	}
+	return (safe_free(save, 0));
 }
 
-int     get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-    static char *save;
-    char        *buf;
-    int         res;
-    ssize_t     n;
+	static char *save;
+	char        *buf;
+	int         res;
+	ssize_t     n;
 
-    n = 0;
-    if (!line || fd < 0 || BUFFER_SIZE <= 0)
-        return (-1);
-    res = 0;
-    if (!(*line = (char*)malloc(sizeof(char) * 1)))
-        return (-1);
-    **line = '\0';
-    if (save)
-        res = put_line(line, &save, save);
-    if (!(buf = malloc(sizeof(char) * ((size_t)BUFFER_SIZE + 1))))
-        res = -1;
-    while (res == 0 && (n = read(fd, buf, BUFFER_SIZE)) > 0)
-    {
-        buf[n] = '\0';
-        if (n == 0 || (res = put_line(line, &save, buf)) == -1)
-            break ;
-    }
-    if (n == -1)
-        res = -1;
-    return (safe_free(&buf, res));
+	n = 0;
+	if (!line || fd < 0 || BUFFER_SIZE <= 0)
+		return (-1);
+	res = 0;
+	if (!(*line = (char*)malloc(sizeof(char) * 1)))
+		return (-1);
+	**line = '\0';
+	if (save)
+		res = put_line(line, &save, save);
+	if (!(buf = malloc(sizeof(char) * ((size_t)BUFFER_SIZE + 1))))
+		res = -1;
+	while (res == 0 && (n = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[n] = '\0';
+		if (n == 0 || (res = put_line(line, &save, buf)) == -1)
+			break ;
+	}
+	if (n == -1)
+		res = -1;
+	return (safe_free(&buf, res));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryishii <ryishii@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: ishiiryoutoku <ishiiryoutoku@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 06:25:46 by ryishii           #+#    #+#             */
-/*   Updated: 2021/03/22 22:48:48 by ryishii          ###   ########.fr       */
+/*   Updated: 2021/03/24 03:26:37 by ishiiryouto      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 # include <sys/uio.h>
 # include <unistd.h>
 
-int             safe_free(char **p, int res);
-int             put_line(char **line, char **save, char *buf);
+int				safe_free(char **p, int res);
+int				put_line(char **line, char **save, char *buf);
 int				get_next_line(int fd, char **line);
 size_t			ft_strlen(const char *s);
 char			*ft_strjoin(const char *s1, const char *s2, size_t n);
